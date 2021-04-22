daf0d449dc567b9f76cd8e47ad5bb0dd0141faf0
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2021-04-22 16:32:08 +0900

    chore(apiclient): remove dispath queue

--- Bucketeer/Sources/APIClient.swift
+++ Bucketeer/Sources/APIClient.swift
@@ -13,7 +13,6 @@ protocol APIClientProtocol {
 final class APIClient: APIClientProtocol {
     private let config: Config
     private let serviceClient: Bucketeer_Gateway_GatewayClient
-    private let queue = DispatchQueue(label: "jp.co.cyberagent.bucketeer.APIQueue", attributes: .concurrent)
     
     init(config: Config) {
         self.config = config
@@ -26,53 +25,49 @@ final class APIClient: APIClientProtocol {
     }
     
     func getEvaluations(userEntity: UserEntity, userEvaluationsId: String, completion: @escaping (Result<(Bucketeer_Gateway_GetEvaluationsResponse?, CallResult), BucketeerError>) -> Void) {
-        queue.async {
-            var request = Bucketeer_Gateway_GetEvaluationsRequest()
-            request.user = userEntity.user
-            request.tag = self.config.tag
-            request.userEvaluationsID = userEvaluationsId
-            let call = self.serviceClient.getEvaluations(request)
-            call.response.and(call.status).whenComplete { result in
-                switch (result) {
-                case .success((let res, let status)):
-                    let callResult = CallResult(success: status.isOk, code: status.code, message: status.message)
-                    if callResult.isFailed {
-                        let message = "(\(Version.number)) Failed to get evaluations: \(callResult)"
-                        Logger.shared.errorLog(message)
-                        completion(.failure(.unknown(message)))
-                        return
-                    }
-                    completion(.success((res, callResult)))
-                case .failure(let error):
-                    let message = "(\(Version.number)) Failed to get evaluations: \(error.localizedDescription)"
+        var request = Bucketeer_Gateway_GetEvaluationsRequest()
+        request.user = userEntity.user
+        request.tag = self.config.tag
+        request.userEvaluationsID = userEvaluationsId
+        let call = self.serviceClient.getEvaluations(request)
+        call.response.and(call.status).whenComplete { result in
+            switch (result) {
+            case .success((let res, let status)):
+                let callResult = CallResult(success: status.isOk, code: status.code, message: status.message)
+                if callResult.isFailed {
+                    let message = "(\(Version.number)) Failed to get evaluations: \(callResult)"
                     Logger.shared.errorLog(message)
                     completion(.failure(.unknown(message)))
+                    return
                 }
+                completion(.success((res, callResult)))
+            case .failure(let error):
+                let message = "(\(Version.number)) Failed to get evaluations: \(error.localizedDescription)"
+                Logger.shared.errorLog(message)
+                completion(.failure(.unknown(message)))
             }
         }
     }
     
     func registerEvents(eventEntities: [EventEntity], completion: @escaping (Result<(Bucketeer_Gateway_RegisterEventsResponse?, CallResult), BucketeerError>) -> Void) {
-        queue.async {
-            var request = Bucketeer_Gateway_RegisterEventsRequest()
-            request.events = eventEntities.compactMap { try? Bucketeer_Event_Client_Event(serializedData: $0.data) }
-            let call = self.serviceClient.registerEvents(request)
-            call.response.and(call.status).whenComplete { result in
-                switch (result) {
-                case .success((let res, let status)):
-                    let callResult = CallResult(success: status.isOk, code: status.code, message: status.message)
-                    if callResult.isFailed {
-                        let message = "(\(Version.number)) Failed to register events: \(callResult)"
-                        Logger.shared.errorLog(message)
-                        completion(.failure(.unknown(message)))
-                        return
-                    }
-                    completion(.success((res, callResult)))
-                case .failure(let error):
-                    let message = "(\(Version.number)) Failed to register events: \(error.localizedDescription)"
+        var request = Bucketeer_Gateway_RegisterEventsRequest()
+        request.events = eventEntities.compactMap { try? Bucketeer_Event_Client_Event(serializedData: $0.data) }
+        let call = self.serviceClient.registerEvents(request)
+        call.response.and(call.status).whenComplete { result in
+            switch (result) {
+            case .success((let res, let status)):
+                let callResult = CallResult(success: status.isOk, code: status.code, message: status.message)
+                if callResult.isFailed {
+                    let message = "(\(Version.number)) Failed to register events: \(callResult)"
                     Logger.shared.errorLog(message)
                     completion(.failure(.unknown(message)))
+                    return
                 }
+                completion(.success((res, callResult)))
+            case .failure(let error):
+                let message = "(\(Version.number)) Failed to register events: \(error.localizedDescription)"
+                Logger.shared.errorLog(message)
+                completion(.failure(.unknown(message)))
             }
         }
     }
