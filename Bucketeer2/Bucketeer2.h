da92fae4c285de271d6362caf233f059b37a81d0
jinSasaki <aysm47@gmail.com>
2022-09-07 00:35:18 +0700

    chore: add Bucketeer2 project and sort

---
+++ Bucketeer2/Bucketeer2.h
@@ -0,0 +1,19 @@
+//
+//  Bucketeer2.h
+//  Bucketeer2
+//
+//  Created by Jin Sasaki on 2022/09/07.
+//  Copyright © 2022 Bucketeer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bucketeer2.
+FOUNDATION_EXPORT double Bucketeer2VersionNumber;
+
+//! Project version string for Bucketeer2.
+FOUNDATION_EXPORT const unsigned char Bucketeer2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bucketeer2/PublicHeader.h>
+
+
---
+++ Bucketeer2Tests/Bucketeer2Tests.swift
@@ -0,0 +1,37 @@
+//
+//  Bucketeer2Tests.swift
+//  Bucketeer2Tests
+//
+//  Created by Jin Sasaki on 2022/09/07.
+//  Copyright © 2022 Bucketeer. All rights reserved.
+//
+
+import XCTest
+@testable import Bucketeer2
+
+class Bucketeer2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
