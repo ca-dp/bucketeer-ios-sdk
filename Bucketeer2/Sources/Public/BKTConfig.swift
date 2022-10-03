49190f3f878343395148047ac2e214f20f800742
Jin Sasaki <aysm47@gmail.com>
2022-10-03 22:44:30 +0900

    fix: remove duplicate constant values

--- Bucketeer2/Sources/Public/BKTConfig.swift
+++ Bucketeer2/Sources/Public/BKTConfig.swift
@@ -1,13 +1,5 @@
 import Foundation
 
-internal let MINIMUM_FLUSH_INTERVAL_MILLIS: Int64 = 60_000 // 60 seconds
-internal let DEFAULT_FLUSH_INTERVAL_MILLIS: Int64 = 60_000 // 60 seconds
-internal let DEFAULT_MAX_QUEUE_SIZE: Int = 50
-internal let MINIMUM_POLLING_INTERVAL_MILLIS: Int64 = 60_000 // 60 seconds
-internal let DEFAULT_POLLING_INTERVAL_MILLIS: Int64 = 600_000 // 10 minutes
-internal let MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS: Int64 = 1_200_000 // 20 minutes
-internal let DEFAULT_BACKGROUND_POLLING_INTERVAL_MILLIS: Int64 = 3_600_000 // 1 hour
-
 public struct BKTConfig {
     let apiKey: String
     let endpoint: URL
@@ -22,10 +14,10 @@ public struct BKTConfig {
         private var apiKey: String?
         private var endpoint: String?
         private var featureTag: String?
-        private var eventsFlushInterval: Int64 = DEFAULT_FLUSH_INTERVAL_MILLIS
-        private var eventsMaxBatchQueueCount: Int = DEFAULT_MAX_QUEUE_SIZE
-        private var pollingInterval: Int64 = DEFAULT_POLLING_INTERVAL_MILLIS
-        private var backgroundPollingInterval: Int64 = DEFAULT_BACKGROUND_POLLING_INTERVAL_MILLIS
+        private var eventsFlushInterval: Int64 = Constant.DEFAULT_FLUSH_INTERVAL_MILLIS
+        private var eventsMaxBatchQueueCount: Int = Constant.DEFAULT_MAX_QUEUE_SIZE
+        private var pollingInterval: Int64 = Constant.DEFAULT_POLLING_INTERVAL_MILLIS
+        private var backgroundPollingInterval: Int64 = Constant.DEFAULT_BACKGROUND_POLLING_INTERVAL_MILLIS
         private var logger: BKTLogger?
 
         public func apiKey(apiKey: String) -> Builder {
@@ -79,17 +71,17 @@ public struct BKTConfig {
                 throw BKTError.illegalArgument(message: "featureTag is required")
             }
 
-            if self.pollingInterval < MINIMUM_POLLING_INTERVAL_MILLIS {
-                logger?.warn(message: "pollingInterval: \(self.pollingInterval) is set but must be above \(MINIMUM_POLLING_INTERVAL_MILLIS)")
-                self.pollingInterval = MINIMUM_POLLING_INTERVAL_MILLIS
+            if self.pollingInterval < Constant.MINIMUM_POLLING_INTERVAL_MILLIS {
+                logger?.warn(message: "pollingInterval: \(self.pollingInterval) is set but must be above \(Constant.MINIMUM_POLLING_INTERVAL_MILLIS)")
+                self.pollingInterval = Constant.MINIMUM_POLLING_INTERVAL_MILLIS
             }
-            if self.backgroundPollingInterval < MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS {
-                logger?.warn(message: "backgroundPollingInterval: \(self.backgroundPollingInterval) is set but must be above \(MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS)")
-                self.backgroundPollingInterval = MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS
+            if self.backgroundPollingInterval < Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS {
+                logger?.warn(message: "backgroundPollingInterval: \(self.backgroundPollingInterval) is set but must be above \(Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS)")
+                self.backgroundPollingInterval = Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS
             }
-            if self.eventsFlushInterval < MINIMUM_FLUSH_INTERVAL_MILLIS {
-                logger?.warn(message: "eventsFlushInterval: \(self.backgroundPollingInterval) is set but must be above \(MINIMUM_FLUSH_INTERVAL_MILLIS)")
-                self.eventsFlushInterval = DEFAULT_FLUSH_INTERVAL_MILLIS
+            if self.eventsFlushInterval < Constant.MINIMUM_FLUSH_INTERVAL_MILLIS {
+                logger?.warn(message: "eventsFlushInterval: \(self.backgroundPollingInterval) is set but must be above \(Constant.MINIMUM_FLUSH_INTERVAL_MILLIS)")
+                self.eventsFlushInterval = Constant.DEFAULT_FLUSH_INTERVAL_MILLIS
             }
 
             return BKTConfig(
