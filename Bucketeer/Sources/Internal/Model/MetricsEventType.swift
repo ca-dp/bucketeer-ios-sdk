75c0f0869f63aa74a2cabc2caf5aa76c6785c97b
jinSasaki <aysm47@gmail.com>
2023-03-30 01:48:03 +0900

    fix: reorder MetricsEventType and use default enum value

--- Bucketeer/Sources/Internal/Model/MetricsEventType.swift
+++ Bucketeer/Sources/Internal/Model/MetricsEventType.swift
@@ -2,16 +2,16 @@ import Foundation
 
 enum MetricsEventType: Int, Codable, Hashable {
     case unknownError = 0
-    case responseLatency = 1
-    case responseSize = 2
-    case timeoutError = 5
-    case networkError = 6
-    case internalError = 7
-    case badRequestError = 8
-    case unauthorizedError = 9
-    case forbiddenError = 10
-    case notFoundError = 11
-    case clientClosedError = 12
-    case unavailableError = 13
-    case internalServerError = 14
+    case responseLatency
+    case responseSize
+    case timeoutError
+    case networkError
+    case internalError
+    case badRequestError
+    case unauthorizedError
+    case forbiddenError
+    case notFoundError
+    case clientClosedError
+    case unavailableError
+    case internalServerError
 }
